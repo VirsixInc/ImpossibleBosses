9de539648f86a5de6d4702c2bdb197295caf234f
Zack <zacharyrock@hotmail.com>
2015-04-03 19:15:02 -0700

    Initial player and camera scripts

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerController : MonoBehaviour {
+
+	CharacterController controller;
+
+	public float speed = 2f;
+
+	void Start () {
+		controller = GetComponent<CharacterController>();
+	}
+
+	void Update () {
+		HandleMovement();
+	}
+
+	void HandleMovement() {
+		Vector3 dir = new Vector3(Input.GetAxis("Vertical"), 0f, Input.GetAxis("Horizontal"));
+
+		controller.Move(dir * speed * Time.deltaTime);
+	}
+}
